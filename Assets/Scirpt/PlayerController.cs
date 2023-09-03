31757ad32c277f8290d4afaecd8a421729cc469e
CongSangDao <sangdaovnb2002@gmail.com>
2023-09-02 21:46:25 -0700

    add turning

--- Assets/Scirpt/PlayerController.cs
+++ Assets/Scirpt/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f; 
+    //speed of go forward
+    private float speed = 15.0f;
+    private float turnSpeed = 60f;
+
+    private float horizontalInput;
+    float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput =  Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         Debug.Log(Time.deltaTime);
         // Vector3.forward -- > (0,0,1)
         // (0,0,1) * Time.deltatime = ( 0,0,.0016) * (0,0,0.16) 
-        transform.Translate( Vector3.forward * Time.deltaTime * speed);     
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //turning vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
